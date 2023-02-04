cb929901da4e23601b4f76a06cb23ff63a078e21
Oleg <nan0machinesun@gmail.com>
2023-02-04 18:09:00 +0200

    Added 'change_permission.sh'

---
+++ change_permission.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# -d    | --base-directory - the base directory from where the recursive search will occur, current directory by default.
+# -ptrn | --patern         - the pattern for files that need to change permissioms, all files by default.
+# -prms | --permissions    - values that will be applied to files that match the pattern, +rw by default.
+
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
+BASE_DIRECTORY="."
+PATTERN="*"
+PERMISSIONS="+rw"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d | --base-directory)
+      BASE_DIRECTORY="$2"
+      shift 2 ;;
+    -ptrn | --pattern)
+      PATTERN="$2"
+      shift 2 ;;
+    -prms | --permissions)
+      PERMISSIONS="$2"
+      shift 2 ;;
+    *)
+      echo "Unknown option $1"
+      exit 1 ;;
+  esac
+done
+
+echo -e "\nPermissions: ${ORANGE}${PERMISSIONS}${NC} will be applied to files that matche pattern: ${ORANGE}${PATTERN}${NC} starting from directory: ${ORANGE}${BASE_DIRECTORY}${NC}\n";
+
+find "${BASE_DIRECTORY}" -name "${PATTERN}" -exec chmod "${PERMISSIONS}" {} \;
