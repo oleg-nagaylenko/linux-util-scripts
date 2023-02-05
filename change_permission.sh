83a2ba93fb6b43e137d31391194312ba9348405e
Oleg <nan0machinesun@gmail.com>
2023-02-05 19:10:10 +0200

    Added messages

--- change_permission.sh
+++ change_permission.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# -d    | --base-directory - the base directory from where the recursive search will occur, current directory by default.
-# -f    | --file-pattern   - the pattern for files that need to change permissioms, all files by default.
-# -p    | --permissions    - values that will be applied to files that match the pattern, +rw by default.
+# -d | --base-directory - the base directory from where the recursive search will occur, current directory by default.
+# -f | --file-pattern   - the pattern for files that need to change permissioms, all files by default.
+# -p | --permissions    - values that will be applied to files that match the pattern, +rw by default.
 
 ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
 BASE_DIRECTORY="."
@@ -30,7 +31,7 @@ done
 
 TARGET_FILES=$(find "${BASE_DIRECTORY}" -type f -name "${PATTERN}")
 
-echo -e "Files: \n$TARGET_FILES\n"
+echo -e "Found files: \n$TARGET_FILES\n"
 
 echo -ne "${ORANGE}Are you sure you wanna change permissions for these files? [y/n] ${NC}"
 read -n 1 -r
@@ -38,5 +39,6 @@ read -n 1 -r
 echo
 if [[ $REPLY = 'y' ]]
 then
-  echo $TARGET_FILES | xargs -t chmod $PERMISSIONS  
+  echo $TARGET_FILES | xargs chmod $PERMISSIONS
+  echo -e "\n${GREEN}File permissions have been changed${NC}\n"  
 fi
